1eae61422fae166c4610d6cc8d765b2629f487dc
Marco Trombino <trombino.marco@gmail.com>
2014-06-23 21:18:06 +0200

    Update luminum.js

--- luminum.js
+++ luminum.js
@@ -3,12 +3,14 @@
 	* Released under the GPLv3 license
 	*
 	* 2013-2014
-	* Version: 1.0
+	* Version: 1.1
 	*/
 	
 	var Luminum = function (color) {
 
 		this.color = color;
+		this.brightness = 0;
+		this.brightnessOffset = 0;
 		this.colorBright = false;
 		this.tollerance = 0;
 		this.color = this.color.replace(/rgb/, "");
@@ -16,23 +18,28 @@
 	};
 	    
 	Luminum.prototype.getColorBrightness = function () {	
-		var brightness;
 	
 		if(this.color.indexOf(',') > -1) {
-		brightness = this.rgbColorBrightness();
+		this.brightness = this.rgbColorBrightness();
 		}
 		else {
-		brightness = this.hexColorBrightness();
+		this.brightness = this.hexColorBrightness();
 		}
 	
-		if(brightness>130) {
+		if(this.brightness>130) {
 		this.colorBright = true;
 		}
-		
+		else {
+		this.colorBright = false;
+		}		
+
+		this.brightnessOffset = this.brightness - (130 - this.tollerance);
+
 		return this.colorBright;
 	};	
 	
 	Luminum.prototype.rgbColorBrightness = function() {	
+		
 		var rgbColor = this.color.split(',');
 		var r = rgbColor[0];
 		var g = rgbColor[1];
@@ -43,11 +50,12 @@
 	};	
 	
 	Luminum.prototype.hexColorBrightness = function() {
+		
 		var bigint = parseInt(this.color, 16);
 		var r = (bigint >> 16) & 255;
 		var g = (bigint >> 8) & 255;
 		var b = bigint & 255;	
-		var brightness = (((r*299) + (g*587) + (b*114)) / 1000) - this.tollerance;
+		var brightness = ((r*299) + (g*587) + (b*114)) / 1000;
 	
 		return brightness;	
 	};
